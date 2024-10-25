7ec03b3d0afae0c95d8da517764aa36a4e634a27
Ozlem Basabakar <ozlembasabakar@gmail.com>
2024-10-26 02:59:01 +0300

    added message box

--- app/src/main/java/com/example/tagginginchat/data/database/UserList.kt
+++ app/src/main/java/com/example/tagginginchat/data/database/UserList.kt
@@ -2,57 +2,68 @@ package com.example.tagginginchat.data.database
 
 import com.example.tagginginchat.R
 import com.example.tagginginchat.data.model.User
+import com.example.tagginginchat.ui.theme.*
 
 val users = mutableListOf<User>(
     User(
         id = 1,
         name = "SpongeBob",
         surname = "SquarePants",
-        profileImage = R.drawable.spongebob
+        profileImage = R.drawable.spongebob,
+        color = SpongeBob
     ),User(
         id = 2,
         name = "Patrick",
         surname = "Star",
-        profileImage = R.drawable.patrick
+        profileImage = R.drawable.patrick,
+        color = Patrick
     ),User(
         id = 3,
         name = "Squidward",
         surname = "Tentacles",
-        profileImage = R.drawable.squidward
+        profileImage = R.drawable.squidward,
+        color = Squidward
     ),User(
         id = 4,
         name = "Eugene H.",
         surname = "Krabs",
-        profileImage = R.drawable.mr_krabs
+        profileImage = R.drawable.mr_krabs,
+        color = MrsKrabs
     ),User(
         id = 5,
         name = "Sheldon J.",
         surname = "Plankton",
-        profileImage = R.drawable.plankton
+        profileImage = R.drawable.plankton,
+        color = Plankton
     ),User(
         id = 6,
         name = "Karen",
         surname = "Plankton",
-        profileImage = R.drawable.karen_plankton
+        profileImage = R.drawable.karen_plankton,
+        color = KarenPlankton
     ),User(
         id = 7,
         name = "Sandy",
         surname = "Cheeks",
-        profileImage = R.drawable.sandy
+        profileImage = R.drawable.sandy,
+        color = Sandy
     ),User(
         id = 8,
         name = "Mrs. Penelope",
         surname = "Puff",
-        profileImage = R.drawable.mrs_puff
+        profileImage = R.drawable.mrs_puff,
+        color = MrsPuff
     ),User(
         id = 9,
         name = "Pearl",
         surname = "Krabs",
-        profileImage = R.drawable.pearl_krabs
+        profileImage = R.drawable.pearl_krabs,
+        color = PearlKrabs
     ),User(
         id = 10,
         name = "Gary",
         surname = "Wilson",
-        profileImage = R.drawable.gary
+        profileImage = R.drawable.gary,
+        color = Gary
     ),
 )
---
+++ app/src/main/java/com/example/tagginginchat/ui/components/MessageBox.kt
@@ -0,0 +1,94 @@
+package com.example.tagginginchat.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tagginginchat.data.database.messages
+import com.example.tagginginchat.data.database.users
+import com.example.tagginginchat.data.model.Message
+import com.example.tagginginchat.ui.theme.ReceivedMessageBackground
+import com.example.tagginginchat.ui.theme.SentMessageBackground
+import com.example.tagginginchat.ui.theme.TaggingInChatTheme
+
+@Composable
+fun MessageBox(modifier: Modifier = Modifier, message: Message) {
+
+    val userInformation = users.find { it.id == message.userId }
+
+    if (userInformation != null) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(if (message.isSent) Alignment.BottomEnd else Alignment.BottomStart)
+        ) {
+            Row(
+                modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = if (message.isSent) Arrangement.End else Arrangement.Start
+            ) {
+                Image(
+                    painter = painterResource(id = userInformation.profileImage),
+                    contentDescription = userInformation.name,
+                    modifier = modifier
+                        .width(40.dp)
+                        .padding(end = 16.dp)
+                        .align(Alignment.Top)
+                )
+                Card(
+                    modifier = modifier
+                        .wrapContentWidth()
+                        .align(Alignment.Top)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .background(if (message.isSent) SentMessageBackground else ReceivedMessageBackground)
+                            .wrapContentWidth()
+
+                    ) {
+                        Text(
+                            text = userInformation.name + " " + userInformation.surname,
+                            color = userInformation.color,
+                            modifier = modifier
+                                .padding(horizontal = 8.dp)
+                                .padding(top = 8.dp, bottom = 4.dp)
+                        )
+                        Text(
+                            text = message.content,
+                            color = Color.White,
+                            modifier = modifier
+                                .padding(horizontal = 8.dp)
+                                .padding(bottom = 8.dp, top = 4.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingMessageBox() {
+    TaggingInChatTheme {
+        MessageBox(
+            modifier = Modifier,
+            message = messages[1],
+        )
+    }
+}
